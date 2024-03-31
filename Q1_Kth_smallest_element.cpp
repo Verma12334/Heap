3cb323792391a22c532a59dd6a140467654d4320
Saurabh <165586434+Verma12334@users.noreply.github.com>
2024-04-01 00:55:34 +0530

    Add files via upload

---
+++ Q1_Kth_smallest_element.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+
+void smallest_element(int arr[],int k){
+
+}
+
+
+
+
+int main(){
+    int arr[6]={-1,4,67,88,35,56};
+    int k;
+    cout<<"Enter the value of k: \n";
+    cin>>k;
+    smallest_element(arr,k);
+
+}
